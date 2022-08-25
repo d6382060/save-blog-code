4198bc9782d6559883a47336853c346d82d4f8f2
djy <852678875@qq.com>
2022-08-25 11:33:25 +0800

    feat: git常用命令

--- deploy/index.js
+++ deploy/index.js
@@ -31,33 +31,39 @@ execSync(`hexo g`, { encoding: 'utf-8', stdio: 'inherit' }) // 重新编译
   const ssh = new NodeSSH()
   const connect = () => {
     ssh
-      .connect(connectOptions)
-      .then(() => {
+      .connect(connectOptions) // 连接服务器，接收服务器用户名和密码端口等
+      .then(() => { // 连接成功
         console.info('Connect success!')
-        uploadFile()
+        uploadFile() // 开始上传
       })
-      .catch(e => {
+      .catch(e => { // 连接失败
         console.error('Connect error:', e)
       })
   }
 
    // 上传文件到服务器
    const uploadFile = () => {
+    // 把第一个参数所在目录的文件上传到 ，第二个参数所在的目录下(服务器目录)
     ssh.putFile(`${deployPath}/public.zip`, `${connectOptions.pathUrl}/public.zip`).then(
       () => {
+        // 上传成功
         console.info('Upload file success')
+        // 解压并且删除上传的zip压缩包文件，解压到cwd目录下
         ssh.execCommand('unzip -o public.zip && rm -rf public.zip', { cwd: connectOptions.pathUrl }).then(result => {
           console.info(`The update message is: ${result.stdout}`)
           if (!result.stderr) {
+            // 解压成功
             console.info('Gratefule! update success!')
-            process.exit(0)
+            process.exit(0) // 退出
           } else {
+            // 解压失败
             console.info('Something wrong:', result)
-            process.exit(0)
+            process.exit(0)  // 退出
           }
         })
       },
       error => {
+        // 上传失败
         console.error('Upload file error:', error)
       }
     )
@@ -65,26 +71,35 @@ execSync(`hexo g`, { encoding: 'utf-8', stdio: 'inherit' }) // 重新编译
 
   // 本地文件压缩
   const zipDirector = () => {
+    // 创建文件输出流
     const output = fs.createWriteStream(`${__dirname}/public.zip`)
     const archive = archiver('zip', {
-      zlib: { level: 3 } // 压缩层级 目前我们只有2层
+      zlib: { level: 3 } // // 设置压缩级别
     }).on('error', err => {
       throw err
     })
+
+    // 文件输出流结束
     output.on('close', err => {
       if (err) {
         console.error('Something error width the zip process:', err)
         return
       }
+      // 连接服务器
       connect()
-      console.info(`${archive.pointer()} total bytes`)
-      console.info('Archiver has been finalized and the output file descriptor has closed.')
+      console.log(`总共 ${archive.pointer()} 字节`)
+      console.log('archiver完成文件的归档，文件输出流描述符已关闭')
     })
+
+    // 数据源是否耗尽
     output.on('end', () => {
-      console.info('Data has been drained')
+      console.log('数据源已耗尽')
     })
+    // 通过管道方法将输出流存档到文件
     archive.pipe(output)
+    // 从子目录追加文件并将其命名为“新子dir”在存档中
     archive.directory(distPath, '/')
+    //完成归档
     archive.finalize()
   }
   zipDirector()
